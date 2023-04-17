6e990c35ccd513413adafa59e4a112b7936c26a2
AmoDinho <u14284783@tuks.co.za>
2023-04-17 21:13:11 +0200

    setting up graphql server

---
+++ stacks/boats/api/index.ts
@@ -0,0 +1,15 @@
+import { Api, use } from 'sst/constructs';
+import { Construct } from 'constructs';
+export interface IBoatsAPIResource {
+  BoatsAPI: Api;
+}
+
+const BoatsAPI = (Stack: Construct): Api => {
+  const boatsAPI = new Api(Stack, `boats-stack-apio`, {
+    routes: {
+      'POST /boats-graphql': {
+        function: 'packages/boats/__graphql/index.handler',
+      },
+    },
+  });
+};
