ac23f977933ad43212d6b7bd6c036a3ac572499c
AmoDinho <u14284783@tuks.co.za>
2023-04-18 21:50:51 +0200

    setting up schema

--- packages/boats/__graphql/builder.ts
+++ packages/boats/__graphql/builder.ts
@@ -1,6 +1,12 @@
 import SchemaBuilder from '@pothos/core';
-
+import { IBoat } from '../types';
 export const builder = new SchemaBuilder({});
 
+const IBoatGQL = builder.objectRef<IBoat>('IBoat');
+IBoatGQL.implement({
+  fields: (t) => ({
+    boat_id: t.exposeString('boat_id'),
+  }),
+});
 builder.queryType({});
 builder.mutationType({});
