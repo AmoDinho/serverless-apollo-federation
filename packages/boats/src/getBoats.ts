aa7faa2e1de2484d817f73eae4459e182abf045a
AmoDinho <u14284783@tuks.co.za>
2023-04-19 21:44:50 +0200

    added types

--- packages/boats/src/getBoats.ts
+++ packages/boats/src/getBoats.ts
@@ -1,6 +1,6 @@
-import BoatsPayload from './data/boats.json';
-
-const getBoats = () => {
-  return BoatsPayload;
+import Boats from '../data/boats.json';
+import { IBoat } from '../types';
+const getBoats = (): Array<IBoat> => {
+  return Boats;
 };
 export default getBoats;
