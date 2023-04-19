a17d73d6634a4906e1ce1346ca8c8c384d3d7ff1
AmoDinho <u14284783@tuks.co.za>
2023-04-19 21:38:11 +0200

    created boat payload

--- packages/boats/__graphql/builder.ts
+++ packages/boats/__graphql/builder.ts
@@ -1,5 +1,6 @@
 import SchemaBuilder from '@pothos/core';
 import { IBoat } from '../types';
+import { getBoats } from '../src/getBoats ';
 export const builder = new SchemaBuilder({});
 
 const IBoatGQL = builder.objectRef<IBoat>('IBoat');
