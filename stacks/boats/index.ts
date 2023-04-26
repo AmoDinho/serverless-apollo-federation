a896b285fb91008d6fb2626f93da2055c2a0bb0b
AmoDinho <u14284783@tuks.co.za>
2023-04-26 21:23:42 +0200

    adding a to-do list

--- stacks/boats/index.ts
+++ stacks/boats/index.ts
@@ -1,14 +1,29 @@
 import { StackContext } from 'sst/constructs';
 import BoatsResourcesAPI from './api';
+import BoatsResoucesDynamoDB from './database';
 
+/*
+Each service needs a stack file where you combine 
+various peices of infrustruce.
+
+Infra to add:
+
+- [ DynamoDB]
+- [ S3]
+- [ SQS]
+
+
+
+*/
 const BoatsStack = ({ stack }: StackContext) => {
   const boatsResourcesAPI = BoatsResourcesAPI(stack);
-
+  const boatsResoucesDynamoDB = BoatsResoucesDynamoDB(stack);
   stack.addOutputs({
     BoatsAPIEndpoint: boatsResourcesAPI.BoatsAPI.url,
   });
   return {
     boatsResourcesAPI,
+    boatsResoucesDynamoDB,
   };
 };
 export default BoatsStack;
