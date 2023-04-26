1682eddfe85945a7dde90f51f163620ca6501c63
AmoDinho <u14284783@tuks.co.za>
2023-04-26 21:59:39 +0200

    added a new type

--- packages/core/types/index.ts
+++ packages/core/types/index.ts
@@ -4,3 +4,11 @@ export type ScalarType<T = void> =
   | boolean
   | Record<string, T>;
 export type RecordScalar<T = void> = Record<string, ScalarType<T>>;
+
+export interface ISignedURLParam {
+  Bucket?: string;
+  Key: string;
+  ACL?: 'public-read';
+  ContentType?: 'application/json' | 'application/pdf';
+  Expires: number;
+}
